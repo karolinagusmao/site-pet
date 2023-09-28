c00108351ff423c4ec686d330f2aa520fe0c2f09
Karolina <karolinab2gusmao@gmail.com>
2023-09-28 17:05:01 -0300

    "using js"

---
+++ PetLife/index.js
@@ -0,0 +1,7 @@
+var elementosDuvida = document.querySelectorAll('.duvida')
+
+elementosDuvida.forEach(function (duvida) {
+    duvida.addEventListener('click', function(){
+        duvida.classList.toggle('ativa')
+    })
+})
